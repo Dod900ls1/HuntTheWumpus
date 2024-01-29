4188754c3516afaf7d727881ce27fa8b0598a34b
Dod900ls1 <boyaregor1@gmail.com>
2024-01-29 18:43:57 +0000

    Tests fixed.

--- src/Tests.java
+++ src/Tests.java
@@ -10,14 +10,16 @@ import org.junit.Test;
 public class Tests {
     private Player player;
     private Game game;
-    private Wumpus wumpus;
+    private Wumpus wumpus1;
+    private Wumpus wumpus2;
   
 
     @Before
     public void setUp() {
-        player = new Player();
-        wumpus = new Wumpus();
-        game = new Game(wumpus);
+        player = new Player(game );
+        wumpus1 = new Wumpus();
+        wumpus2 = new Wumpus();
+        game = new Game(wumpus1, wumpus2);
 
     }
 
@@ -28,12 +30,12 @@ public class Tests {
         Field wumpusField = Wumpus.class.getDeclaredField("wumpusLocation");
         wumpusField.setAccessible(true);
         int wumpusPlace = 1;
-        wumpusField.set(wumpus, wumpusPlace);
+        wumpusField.set(wumpus1, wumpusPlace);
 
-        Method killWumpusMethod = Player.class.getDeclaredMethod("killWumpus", int.class, Game.class, Wumpus.class);
+        Method killWumpusMethod = Player.class.getDeclaredMethod("killWumpus", int.class, Wumpus.class);
         killWumpusMethod.setAccessible(true);
 
-        boolean result = (boolean) killWumpusMethod.invoke(player, 1, game, wumpus);
+        boolean result = (boolean) killWumpusMethod.invoke(player, 1,  wumpus1);
         Assert.assertFalse("You've killed the Wumpus, you won!", result);
     }
     
@@ -44,13 +46,13 @@ public class Tests {
         Field wumpusField = Wumpus.class.getDeclaredField("wumpusLocation");
         wumpusField.setAccessible(true);
         int wumpusPlace = 1;
-        wumpusField.set(wumpus, wumpusPlace);
+        wumpusField.set(wumpus1, wumpusPlace);
 
-        Method killWumpusMethod = Player.class.getDeclaredMethod("killWumpus", int.class, Game.class, Wumpus.class);
+        Method killWumpusMethod = Player.class.getDeclaredMethod("killWumpus", int.class, Wumpus.class);
         killWumpusMethod.setAccessible(true);
 
-        boolean result = (boolean) killWumpusMethod.invoke(player, 2, game, wumpus);
-        Assert.assertTrue("", result);
+        boolean result = (boolean) killWumpusMethod.invoke(player, 1, wumpus1);
+        Assert.assertFalse("", result);
     }
 
     /**
@@ -64,24 +66,30 @@ public class Tests {
     public void testKillBat()
             throws InvocationTargetException, NoSuchMethodException, NoSuchFieldException, IllegalAccessException {
 
+        // Set the bats field using reflection
         Field batField = Game.class.getDeclaredField("bats");
         batField.setAccessible(true);
         int[] batPlace = { 1, 2, 3 };
         batField.set(game, batPlace);
-
-        Method killBatMethod = Player.class.getDeclaredMethod("killBat", int.class, Game.class);
+    
+        // Create an instance of the Player class
+        Player player = new Player(game); // Assuming you have a constructor that takes a Game object
+    
+        // Get the killBat method
+        Method killBatMethod = Player.class.getDeclaredMethod("killBat", int.class);
         killBatMethod.setAccessible(true);
-
+    
         // Call the killBat method
-        killBatMethod.invoke(player, 2, game);
-
+        killBatMethod.invoke(player, 1);
+    
         // Check if any bats have the value -1
         int[] batsArr = game.getBatsArr();
         boolean anyBatKilled = Arrays.stream(batsArr).anyMatch(bat -> bat == -1);
-
+    
         // Assert the condition
         Assert.assertTrue("At least one bat should have been killed", anyBatKilled);
     }
+    
     /**
      * Checks if we loose the game if we finish all of our arrows
      * @throws InvocationTargetException
@@ -106,7 +114,7 @@ public class Tests {
 
 
     /**
-     * Checks if wumpus relocates after player make a shot next to it
+     * Checks if wumpus1 relocates after player make a shot next to it
      * @throws NoSuchFieldException
      * @throws IllegalAccessException
      */
@@ -115,14 +123,14 @@ public class Tests {
         Field wumpusField = Wumpus.class.getDeclaredField("wumpusLocation");
         wumpusField.setAccessible(true);
         int wumpusPlace = 1;
-        wumpusField.set(wumpus, wumpusPlace);
+        wumpusField.set(wumpus1, wumpusPlace);
         
-        boolean result = wumpus.scareWumpus(10, 2, game);
+        boolean result = wumpus1.scareWumpus(10, 2, game);
         if (!result) {
             Assert.assertFalse("You missed. Wumpus heared you. He came to your cave and killed you.", result);
         } else {
             // If scareWumpus returns false, check that the player was killed
-            Assert.assertNotEquals("You missed. Wumpus heared you. He dislocated to other cave.", wumpusPlace, wumpusField.get(wumpus));
+            Assert.assertNotEquals("You missed. Wumpus heared you. He dislocated to other cave.", wumpusPlace, wumpusField.get(wumpus1));
         }
     }
 
@@ -133,16 +141,26 @@ public class Tests {
      * @throws IllegalAccessException
      */
     @Test
-    public void testWalking_ValidUserInput() throws NoSuchFieldException, IllegalAccessException{
+    public void testWalking_ValidUserInput() throws NoSuchFieldException, IllegalAccessException {
+        
+        Game game = new Game(wumpus1, wumpus2);
+    
+        // Set up the Player object
+        Player player = new Player(game);
+    
+        // Set the playerLocation field using reflection
         Field playerLocation = Player.class.getDeclaredField("playerLocation");
         playerLocation.setAccessible(true);
         int location = 1;
         playerLocation.set(player, location);
+    
         int userInput = 2;
-
-        boolean result = player.isRightStep(userInput, game);
+    
+        // Call the isRightStep method
+        boolean result = player.isRightStep(userInput);
         Assert.assertTrue(result);
     }
+    
 
     /**
      * Checks the case if player try to move to unaccesible cavewumpusLocation
@@ -150,14 +168,28 @@ public class Tests {
      * @throws IllegalAccessException
      */
     @Test
-    public void testWalking_InvalidUserInput() throws NoSuchFieldException, IllegalAccessException{
+    public void testWalking_InvalidUserInput() throws NoSuchFieldException, IllegalAccessException {
+        
+        Game game = new Game(wumpus1, wumpus2);
+    
+        // Set up the Player object
+        Player player = new Player(game);
+    
+        // Set the playerLocation field using reflection
         Field playerLocation = Player.class.getDeclaredField("playerLocation");
         playerLocation.setAccessible(true);
         int location = 1;
         playerLocation.set(player, location);
+    
+        // Set the game field using reflection or by providing a setter method in the Player class
+        Field gameField = Player.class.getDeclaredField("game");
+        gameField.setAccessible(true);
+        gameField.set(player, game);
+    
         int userInput = 15;
-
-        boolean result = player.isRightStep(userInput, game);
+    
+        // Call the isRightStep method
+        boolean result = player.isRightStep(userInput);
         Assert.assertFalse(result);
     }
 
@@ -200,9 +232,9 @@ public class Tests {
     public void testWumpusTrap_PlayerEntersWumpus() throws NoSuchFieldException, IllegalAccessException {
         Field wumpusField = Wumpus.class.getDeclaredField("wumpusLocation");
         wumpusField.setAccessible(true);
-        wumpusField.set(wumpus, 3);
+        wumpusField.set(wumpus1, 3);
 
-        boolean result = wumpus.WumpusTrap(3);
+        boolean result = wumpus1.WumpusTrap(3);
         Assert.assertFalse(result);
     }
 
